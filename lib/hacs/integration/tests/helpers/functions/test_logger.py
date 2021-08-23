668d4e1ad13f39fb0ac68962ee56cae553e59445
zhorvath83 <zhorvath83@gmail.com>
2021-08-23 12:27:41 +0200

    sub

--- lib/hacs/integration/tests/helpers/functions/test_logger.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from custom_components.hacs.utils.logger import getLogger
-
-
-def test_logger():
-    os.environ["GITHUB_ACTION"] = "value"
-    getLogger()
-    del os.environ["GITHUB_ACTION"]
